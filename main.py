1495579cc3b1ed5c7fe73005c6e776415c1dfcc7
voidarchive <voidarchived@gmail.com>
2021-12-21 17:27:22 +0545

    new_post

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ from flask_ckeditor import CKEditor, CKEditorField
 
 
 ## Delete this code:
-# import requests
-# posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -32,27 +31,28 @@ class BlogPost(db.Model):
     img_url = db.Column(db.String(250), nullable=False)
 
 
+
+
 ##WTForm
 class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
     author = StringField("Your Name", validators=[DataRequired()])
     img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
-    body = StringField("Blog Content", validators=[DataRequired()])
+
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
 
 
 @app.route('/')
 def get_all_posts():
+    posts = db.session.query(BlogPost).all()
     return render_template("index.html", all_posts=posts)
 
 
-@app.route("/post/<int:index>")
-def show_post(index):
-    requested_post = None
-    for blog_post in posts:
-        if blog_post["id"] == index:
-            requested_post = blog_post
+@app.route("/post/<int:post_id>")
+def show_post(post_id):
+    requested_post = db.session.query(BlogPost).get(post_id)
     return render_template("post.html", post=requested_post)
 
 
@@ -64,6 +64,16 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.route("/edit_post")
+def edit_post():
+    pass
+
+
+@app.route('/new-post',methods=['GET','POST'])
+def new_post():
+    form = CreatePostForm()
+    render_template('make-post.html', form=form)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
